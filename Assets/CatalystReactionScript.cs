3bf34e8fc5956d8f39a07b0ac65b957885323b01
ssogo <43896422+sgsogo@users.noreply.github.com>
2021-08-31 07:23:37 -0700

    AnimatedComplex2 added

--- Assets/CatalystReactionScript.cs
+++ Assets/CatalystReactionScript.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CatalystReactionScript : MonoBehaviour
 {
-    public GameObject CatalystReactionProduct;
+    public GameObject CatalystReactionProduct;  //This is the Animated Complex For Step #X  (e.g. Step1AnimatedComplex,  Step2AnimatedComplex)
     public Vector2 CatalystPosition;
-    public Animator Step1Animation;
+    public Animator AnimatedComplexForThisStep;
+    public string SubstrateTag;
 
 
     // Start is called before the first frame update
@@ -21,19 +22,26 @@ public class CatalystReactionScript : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D otherCollider)
+    private void OnTriggerEnter2D(Collider2D Substrate)
     {
-        print("other collider = " + otherCollider);
-        CatalystPosition = gameObject.transform.position;
-        Instantiate(CatalystReactionProduct, CatalystPosition, Quaternion.identity);
-        //otherCollider.GetComponent<SubstrateMoleculeScript>().SpawnProductMolecule();
-        Step1Animation = GameObject.FindGameObjectWithTag("AnimatedComplex").GetComponent<Animator>();
+        print("Substrate = " + Substrate);
+        print("Substrate tag = " + Substrate.tag);
 
-        Step1Animation.SetTrigger("StartTheAnimation");
+        if(Substrate.tag == SubstrateTag)
+        {
+            CatalystPosition = gameObject.transform.position;
+            Instantiate(CatalystReactionProduct, CatalystPosition, Quaternion.identity);
+            //otherCollider.GetComponent<SubstrateMoleculeScript>().SpawnProductMolecule();  //no longer valid, as the animated complex is a single GameObject containing both catalyst and substrate
+            AnimatedComplexForThisStep = GameObject.FindGameObjectWithTag("AnimatedComplex").GetComponent<Animator>();
 
+            AnimatedComplexForThisStep.SetTrigger("StartTheAnimation");
 
-        //Destroy(gameObject);
-        Destroy(otherCollider.gameObject);
+            Destroy(gameObject);   //this destroys the "CAT" or CAT complex
+            Destroy(Substrate.gameObject);  //this destroys the substrate molecule, as both are replaced by the animated complex
+
+        }
+      
+        
     }
 
 
--- Assets/WhenAnimationIsDone.cs
+++ Assets/WhenAnimationIsDone.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhenAnimationIsDone : MonoBehaviour
+public class WhenAnimationIsDone : MonoBehaviour  //THIS SCRIPT IS ATTACHED TO THE ANIMATED COMPLEXES (e.g. Step1AnimatedComplex).  Set "NextCatalystModel" in the prefab Animated Complex
 {
-    public GameObject CatalystOH;
+    public GameObject NextCatalystModel;
+    public Vector3 NextCatalystModelOffset;
+    public GameObject NextSubstrateModel;
+    public Vector3 NextSubstrateModelOffset;
+    public Vector3 EulerAnglesForSubstrateModel;
+    public GameObject ProductMoleculeCompleted;
 
 
     // Start is called before the first frame update
@@ -23,10 +28,22 @@ public class WhenAnimationIsDone : MonoBehaviour
     public void AnimationFinished()
     {
         print("Animation is finished");
-        Instantiate(CatalystOH);
-        //Destroy(gameObject);
+        Vector2 NextCatalystModelPosition = gameObject.transform.position + NextCatalystModelOffset;  //new Vector3(-0.54f, 2.0f, 0);
+        Instantiate(NextCatalystModel, NextCatalystModelPosition, Quaternion.identity);
+        print("instantiated CAT at " + NextCatalystModelPosition);
+
+        Vector2 NextSubstrateModelPosition = gameObject.transform.position + NextSubstrateModelOffset;
+        Instantiate(NextSubstrateModel, NextSubstrateModelPosition, Quaternion.Euler(EulerAnglesForSubstrateModel));
+
+        if(ProductMoleculeCompleted != null)
+        {
+            Instantiate(ProductMoleculeCompleted);
+        }
+
+        Destroy(gameObject);
 
         //NEED TO INSTANTIATE CATALYST-OH GAME OBJECT WITH OFFSET (-0.54, 2.0) RELATIVE TO POSITION OF ANIMATED COMPLEX.  
+
         //ALSO NEED TO INSTANTIATE OH RADICAL(B) WITH OFFSET (2, 6.5) RELATIVE TO POSITION OF ANIMATED COMPLEX
     }
 }
